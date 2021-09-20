5030b1790b03178cb66e3e458f54843ee057862f
dressalsl <dressalsl97@gmail.com>
2021-09-19 22:38:11 -0300

    entrega3

--- atv_2/startup_namer/lib/main.dart
+++ atv_2/startup_namer/lib/main.dart
@@ -25,9 +25,10 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
+  final _suggestions = <String>[];
+  final _saved = <String>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,45 +43,6 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildSuggestions() {
-    Widget _buildRow(WordPair pair) {
-      final alreadySaved = _saved.contains(pair);
-      return Dismissible(
-          direction: DismissDirection.endToStart,
-          key: Key(pair.asPascalCase),
-          child: ListTile(
-            title: Text(
-              pair.asPascalCase,
-              style: _biggerFont,
-            ),
-            trailing: Icon(
-              alreadySaved ? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved ? Colors.red : null,
-            ),
-            onTap: () {
-              setState(() {
-                if (alreadySaved) {
-                  _saved.remove(pair);
-                } else {
-                  _saved.add(pair);
-                }
-              });
-            },
-          ),
-          background: Container(
-              color: Colors.red,
-              child: Align(
-                  alignment: AlignmentDirectional.centerEnd,
-                  child: Icon(Icons.delete_forever))),
-          onDismissed: (direction) {
-            setState(() {
-              _suggestions.remove(pair);
-              if (alreadySaved) {
-                _saved.remove(pair);
-              }
-            });
-          });
-    }
-
     return ListView.builder(
         padding: const EdgeInsets.all(16),
         itemBuilder: (BuildContext _context, int i) {
@@ -89,9 +51,51 @@ class _RandomWordsState extends State<RandomWords> {
           }
           final int index = i ~/ 2;
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
+            _suggestions.add(WordPair.random().asPascalCase);
           }
-          return _buildRow(_suggestions[index]);
+          return _buildRow(_suggestions[index], index);
+        });
+  }
+
+  Widget _buildRow(String name, int index) {
+    final alreadySaved = _saved.contains(name);
+    return Dismissible(
+        direction: DismissDirection.endToStart,
+        key: Key(name),
+        child: ListTile(
+          title: Text(
+            name,
+            style: _biggerFont,
+          ),
+          trailing: IconButton(
+            icon: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border),
+            color: alreadySaved ? Colors.pink : null,
+            onPressed: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(name);
+                } else {
+                  _saved.add(name);
+                }
+              });
+            },
+          ),
+          onTap: () {
+            _editRow(name, index);
+          },
+        ),
+        background: Container(
+            color: Colors.red,
+            child: Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Icon(Icons.delete_forever))),
+        onDismissed: (direction) {
+          setState(() {
+            _suggestions.remove(name);
+            if (alreadySaved) {
+              _saved.remove(name);
+            }
+          });
         });
   }
 
@@ -100,10 +104,10 @@ class _RandomWordsState extends State<RandomWords> {
       MaterialPageRoute<void>(
         builder: (BuildContext context) {
           final tiles = _saved.map(
-            (WordPair pair) {
+            (name) {
               return ListTile(
                 title: Text(
-                  pair.asPascalCase,
+                  name,
                   style: _biggerFont,
                 ),
               );
@@ -123,5 +127,67 @@ class _RandomWordsState extends State<RandomWords> {
         }, // ...to here.
       ),
     );
+  }
+
+  void _saveEdit(BuildContext context) {
+    if (!_formKey.currentState!.validate()) return;
+    setState(() {
+      _formKey.currentState!.save();
+    });
+    Navigator.pop(context);
+  }
+
+  void _editRow(String name, int index) {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(title: const Text('Go back')),
+          body: Wrap(
+            children: <Widget>[
+              Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    validator: (value) {
+                      return value!.isEmpty ? 'Can not be null' : null;
+                    },
+                    autofocus: true,
+                    initialValue: name,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      contentPadding: const EdgeInsets.only(
+                          left: 15, top: 15, right: 15, bottom: 15),
+                    ),
+                    onSaved: (value) {
+                      _suggestions[index] = value.toString();
+                      if (_saved.contains(name)) {
+                        _saved.remove(name);
+                        _saved.add(value.toString());
+                      }
+                    },
+                  )),
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () => _saveEdit(context),
+                  child: Text('Save'),
+                  style: TextButton.styleFrom(
+                    primary: Colors.pink,
+                    padding: EdgeInsets.all(4),
+                    side: BorderSide(
+                      color: Colors.pink,
+                    ),
+                    backgroundColor: Colors.pink.shade100,
+                    minimumSize: Size(200, 50),
+                  ),
+                ),
+              )
+            ],
+          ));
+    }));
   }
 }
